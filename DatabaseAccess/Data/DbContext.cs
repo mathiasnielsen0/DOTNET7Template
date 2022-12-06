b40da372f6adf8c0b36940353f82dd79e3fd3b92
mathiasnielsen <mathiasnielsen0@gmail.com>
2022-12-06 23:16:54 +0100

    Indledende templatestruktur

---
+++ DatabaseAccess/Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAccess.Data;
+
+public class DbContext : IdentityDbContext<Bruger>
+{
+    public DbContext(DbContextOptions<DbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Website/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Website.Infrastructure;
+
+namespace Website.Controllers;
+
+[Authorize]
+public class AccountController : BaseController
+{
+    public AccountController()
+    {
+        
+    }
+
+    public async Task<IActionResult> Register()
+    {
+
+        return View();
+    }
+    
+    public async Task<IActionResult> Login()
+    {
+
+        return View();
+    }
+    
+    public async Task<IActionResult> SignOut()
+    {
+
+        return View();
+    }
+    
+}
