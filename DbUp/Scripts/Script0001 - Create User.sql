ab6682b634b3b5c597fbbed40ec975103d833906
mathiasnielsen <mathiasnielsen0@gmail.com>
2022-12-06 23:19:00 +0100

    script

--- DbUp/Scripts/Script0001 - Create User.sql
+++ DbUp/Scripts/Script0001 - Create User.sql
@@ -1,9 +1,9 @@
-CREATE TABLE [dbo].[Bruger]
-    ADD [ID] INT NOT NULL IDENTITY(1,1), 
+CREATE TABLE [dbo].[Bruger](
+    [ID] INT NOT NULL IDENTITY(1,1), 
     [Navn] NVARCHAR(255) NOT NULL,
     [Email] NVARCHAR(255) NOT NULL,
     [ResetPasswordGuid] UUID NULL,
     [PasswordHash] NVARCHAR(255) NOT NULL,
     [Salt] NVARCHAR(255) NOT NULL,
-    [Administrator] BIT NOT NULL DEFAULT 0,
-    
+    [Administrator] BIT NOT NULL DEFAULT 0   
+)
