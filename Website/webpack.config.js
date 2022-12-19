bcdeb4c26541180005c43c7e8fd7a71bffe35ba4
mathiasnielsen <mathiasnielsen0@gmail.com>
2022-12-19 23:20:22 +0100

    Fjernet bootstrap og bruger webpack og node

---
+++ Website/webpack.config.js
@@ -0,0 +1,32 @@
+﻿'use strict'
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: {
+        site: './src/js/index.js'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, 'wwwroot', 'dist')
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader'],
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
+};
